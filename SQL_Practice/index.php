fae155101191db6f193ccedf2b76e9efe21720cf
Hexen <koydano@gmail.com>
2026-01-20 18:28:04 +0100

    auth-windov almost done

--- SQL_Practice/index.php
+++ SQL_Practice/index.php
@@ -21,17 +21,17 @@ echo "<br>";
 		
 
 
-		<div class="login_window_bagraund active">
-			<div class="login_window_wrapper">
+		<div class="auth-overlay active">
+			<div class="auth-card">
 				<div class="auth-tabs">
 					<ul>
-						<li><button type="button">authorization</button></li>
+						<li><button class="is-active" type="button">authorization</button></li>
 						<li><button type="button">registration</button></li>
 					</ul>
 				</div>
 				
 				<div class="auth-content">
-					<form class="auth-window" id="auth_window" name="auth_window" action="" method="post">
+					<form class="auth-form-login" id="auth-form-login" name="auth-form-login" action="" method="post">
 						
 						<label for="user_login">Login</label>
 						<input 
@@ -45,10 +45,17 @@ echo "<br>";
 							id="user_pas"
 							name="user_pas"
 							placeholder="******">
-						<button type="submit">send</button>			
+						<div class="auth-spacer"></div>
+						<!-- <label class="is-hiden" for="new_user_pas_rep ">auth-spacer</label>
+						<input 
+							class="auth-spacer is-hiden"
+							type="text"
+							placeholder="******"> -->
+						<br>
+						<button class="btn-main" type="submit">send</button>			
 					</form>
 
-					<form class="reg_window" name="reg_window" id="reg_window" action="" method="post">
+					<form class="auth-form-reg is-hiden" name="reg_window" id="reg_window" action="" method="post">
 					
 						<label for="new_user_login">Login</label>
 						<input 
@@ -69,14 +76,14 @@ echo "<br>";
 							id="new_user_pas_rep"
 							placeholder="******">
 						<br>
-						<button type="submit">registration user</button>
+						<button class="btn-main" type="submit">registration user</button>
 					</form>
 
 				</div>
 			</div>
 		</div>
 	
-		<div class="user_window is-hidden" id="user_panel">
+		<div class="user_window is-none" id="user_panel">
 			<h2>Welcome, <span class="user_name"></span></h2>
 			<div class="personal_data">
 				<h3>Personal data</h3>
@@ -96,7 +103,7 @@ echo "<br>";
 			</div>
 		</div>
 
-		<div class="admin_window is-hidden" id="admin_panel">
+		<div class="admin_window is-none" id="admin_panel">
 
 		</div>
 
